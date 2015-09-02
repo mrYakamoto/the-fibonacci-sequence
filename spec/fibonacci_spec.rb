c7d26abba59d96ca5c697a2ed6f80654cf965f23
Torey Hickman <torey@toreyhickman.com>
2015-09-02 08:57:53 -0500

    Refactor tests

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,28 +1,39 @@
 require_relative('../fibonacci')
 
 describe 'iterative_nth_fibonacci_number' do
-  it 'should calculate the 0th fibonacci number' do
-   expect(iterative_nth_fibonacci_number(0)).to eq(0)
+  it 'calculates the 0th fibonacci number' do
+    calculated_number = iterative_nth_fibonacci_number(0)
+    expect(calculated_number).to eq 0
   end
 
-  it 'should calculate the 1st fibonacci number' do
-   expect(iterative_nth_fibonacci_number(1)).to eq(1)
+  it 'calculates the 1st fibonacci number' do
+    calculated_number = iterative_nth_fibonacci_number(1)
+    expect(calculated_number).to eq 1
   end
 
-  it 'should calculate the 2nd fibonacci number' do
-   expect(iterative_nth_fibonacci_number(2)).to eq(1)
+  it 'calculates the 2nd fibonacci number' do
+    calculated_number = iterative_nth_fibonacci_number(2)
+    expect(calculated_number).to eq 1
   end
 
-  it 'should calculate the 3rd fibonacci number' do
-   expect(iterative_nth_fibonacci_number(3)).to eq(2)
+  it 'calculates the 3rd fibonacci number' do
+    calculated_number = iterative_nth_fibonacci_number(3)
+    expect(calculated_number).to eq 2
   end
 
-  it 'should calculate the 4th fibonacci number' do
-   expect(iterative_nth_fibonacci_number(4)).to eq(3)
+  it 'calculates the 4th fibonacci number' do
+    calculated_number = iterative_nth_fibonacci_number(4)
+    expect(calculated_number).to eq 3
   end
 
-  it 'should calculate the 5th fibonacci number' do
-   expect(iterative_nth_fibonacci_number(5)).to eq(5)
+  it 'calculates the 5th fibonacci number' do
+    calculated_number = iterative_nth_fibonacci_number(5)
+    expect(calculated_number).to eq 5
+  end
+
+  it 'calculates the 17th fibonacci number' do
+    calculated_number = iterative_nth_fibonacci_number(17)
+    expect(calculated_number).to eq 1597
   end
 end
 
